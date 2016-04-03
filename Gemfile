f128da50e73c54e4ff7c99bb240d68e27d9dc7fb
jojo <daorenmc@gmail.com>
2016-04-03 11:46:17 +0800

    class_videos CRUD

--- Gemfile
+++ Gemfile
@@ -22,12 +22,12 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+gem 'haml', '~> 4.0', '>= 4.0.7'
+gem 'simple_form', '~> 3.2'
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.5.1'
+
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
-gem 'bootstrap-sass'
-gem 'haml'
-gem 'simple_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/class_videos_controller.rb
+++ app/controllers/class_videos_controller.rb
@@ -2,19 +2,25 @@ class ClassVideosController < ApplicationController
   before_action :find_video, only: [:show, :edit, :update, :destroy]
 
   def index
-    @class_videos = ClassVideo.all.order('created_at DESC')
-  end
-
-  def new
-
-  end
-
-  def create
-    #code
+    @class_videos = ClassVideo.all.order('created_at ASC')
   end
 
   def show
 
+  end
+
+  def new
+    @class_video = ClassVideo.new
+  end
+
+  def create
+    @class_video = ClassVideo.new video_params
+
+    if @class_video.save!
+      redirect_to @class_video, notice: "Successfully create new class_videos"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -22,14 +28,24 @@ class ClassVideosController < ApplicationController
   end
 
   def update
-
+    if @class_video.update(video_params)
+      redirect_to @class_video
+    else
+      render 'edit' # render 带参数，所以回来的时候，之前填写的数据不会丢失
+    end
   end
 
   def destroy
-    #code
+    @class_video.destroy
+    redirect_to root_path, notice: "Successfully delete the class video"
   end
 
   private
+
+  def video_params
+    params[class_video].permit(:title, :speakers, :cover_image, :play_count, :video, :description)
+  end
+
   def find_video
     @class_video = ClassVideo.find(params[:id])
   end
